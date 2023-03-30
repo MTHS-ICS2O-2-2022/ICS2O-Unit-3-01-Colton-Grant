a8fdd6519d8ae7a0bfc4f9db8cdeb70680cbf48e
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-03-30 16:09:05 +0000

    addeed storyboard

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: colton
+// Created on: march2023
 // This file contains the JS functions for index.html
+'use strict'
+/**
+* This function calculates area and perimeter of rectangle.
+*/
+function doMATH() {
+ // input
+ const baseA = parseInt(document.getElementById('A-base').value)
+ const baseB= parseInt(document.getElementById('B-base').value)
+ const height= parseInt(document.getElementById('Height').value)
+
+
+ // process
+ const area = (baseA + baseB) / 2 * Height
+
+
+ // output
+ document.getElementById('area').innerHTML =
+   'Area is: ' + (baseA + baseB)/2 * height + ' cm²'
+}
